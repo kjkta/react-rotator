2e2b1da654d84401c253a8e4015653ef2cb64ac7
Kyle Thomson <kjay.thomson@gmail.com>
2019-07-31 14:36:47 +0200

    Modulize

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# react-rotator
+# useRotator
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rotator)
+Rotate an element with a handle
+
+```jsx
+function App() {
+  const [rotatableRef, handleRef, degree, isDragging] = useRotator(
+    initialDegree,
+    enableDragging
+  );
+  return (
+    <div>
+      <div ref={handleRef} style={{ cursor: "grab" }}>
+        Grab me to rotate
+      </div>
+      <div ref={rotatableRef} style={{ transform: `translate(${degree}deg)` }}>
+        I can be rotated!
+      </div>
+    </div>
+  );
+}
+```
--- index.js
+++ index.js
@@ -1,54 +1,62 @@
-import React from 'react';
-import useMouse from "@rooks/use-mouse"
+import React from "react";
+import useMouse from "@rooks/use-mouse";
 
 export default function useRotator(initialDegree, enableDragging) {
-  const ref = React.useRef(null)
+  const ref = React.useRef(null);
   const [degree, setDegree] = React.useState(initialDegree);
-  const mousePos = useMouse()
-  const handleRef = React.useRef(null)
-  const isDragging = useDragging(handleRef.current)
+  const mousePos = useMouse();
+  const handleRef = React.useRef(null);
+  const isDragging = useDragging(handleRef.current);
 
-  React.useEffect(function () {
-    if (ref.current && enableDragging && isDragging) {
-      const rect = ref.current.getBoundingClientRect()
-      const centerX = (rect.left + rect.right) / 2
-      const centerY = (rect.top + rect.bottom) / 2
+  React.useEffect(
+    function() {
+      if (ref.current && enableDragging && isDragging) {
+        const rect = ref.current.getBoundingClientRect();
+        const centerX = (rect.left + rect.right) / 2;
+        const centerY = (rect.top + rect.bottom) / 2;
 
-      const radians = Math.atan2(mousePos.clientX - centerX, mousePos.clientY - centerY)
-      const degree = Math.round((radians * (180 / Math.PI) * -1) + 100) + 80;
-      setDegree(degree)
-    }
-  }, [mousePos, ref.current, isDragging])
+        const radians = Math.atan2(
+          mousePos.clientX - centerX,
+          mousePos.clientY - centerY
+        );
+        const degree = Math.round(radians * (180 / Math.PI) * -1 + 100) + 80;
+        setDegree(degree);
+      }
+    },
+    [mousePos, ref.current, isDragging]
+  );
 
-  return [ref, handleRef, degree, isDragging]
+  return [ref, handleRef, degree, isDragging];
 }
 
+const MOUSE_DOWN = "MOUSE_DOWN";
+const MOUSE_MOVE = "MOUSE_MOVE";
+const MOUSE_UP = "MOUSE_UP";
 function useDragging(el) {
-  const [mouseDown, setMouseDown] = React.useState(false)
-  const [moving, setMoving] = React.useState(false);
-
-  const MOUSE_DOWN = "MOUSE_DOWN"
-  const MOUSE_MOVE = "MOUSE_MOVE"
-  const MOUSE_UP = "MOUSE_UP"
-  const [{ mouseDown, mouseDragging }, dispatch] = React.useReducer(function (state, action) {
-    switch (action.type) {
-      case MOUSE_DOWN: return { mouseDown: true, mouseDragging: false };
-      case MOUSE_MOVE: return { ...state, mouseDragging: state.mouseDown ? true : false };
-      case MOUSE_UP: return { mouseDown: false, mouseDragging: false }
-    }
-  }, { mouseDown: false, mouseDragging: false })
-
+  const [{ mouseDown, mouseDragging }, dispatch] = React.useReducer(
+    function(state, action) {
+      switch (action.type) {
+        case MOUSE_DOWN:
+          return { mouseDown: true, mouseDragging: false };
+        case MOUSE_MOVE:
+          return { ...state, mouseDragging: state.mouseDown ? true : false };
+        case MOUSE_UP:
+          return { mouseDown: false, mouseDragging: false };
+      }
+    },
+    { mouseDown: false, mouseDragging: false }
+  );
 
   function handleMouseDown() {
-    dispatch({ type: MOUSE_DOWN })
+    dispatch({ type: MOUSE_DOWN });
   }
 
   function handleMouseMove() {
-    dispatch({ type: MOUSE_MOVE })
+    dispatch({ type: MOUSE_MOVE });
   }
 
   function handleMouseUp() {
-    dispatch({ type: MOUSE_UP })
+    dispatch({ type: MOUSE_UP });
   }
 
   React.useEffect(() => {
@@ -67,5 +75,5 @@ function useDragging(el) {
     };
   }, [el]);
 
-  return mouseDragging
+  return mouseDragging;
 }
